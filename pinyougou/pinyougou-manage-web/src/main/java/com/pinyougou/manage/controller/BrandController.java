9c9bf0425ec3153dc31f5c045d0ea6069d6abd74
LAPTOP-V82HOOT4\User <David_lzt@163.com>
2020-06-19 23:58:19 +0800

    品牌列表增删改查

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -21,19 +21,35 @@ public class BrandController {
     @Reference
     private BrandService brandService;
 
-    @GetMapping("/findPage")
-    public PageResult findPage(@RequestParam(value = "page", defaultValue = "1") Integer page,@RequestParam(value = "rows", defaultValue ="10") Integer rows) {
-        return brandService.findPage(page, rows);
+    /**
+     * 根据条件分页查询
+     *
+     * @param brand 查询条件
+     * @param page  页号
+     * @param rows  页大小
+     * @return
+     */
+    @PostMapping("/search")
+    public PageResult search(@RequestBody TbBrand brand, @RequestParam(value ="page", defaultValue = "1") Integer page,
+                             @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+        return brandService.search(brand, page, rows);
     }
+
+   /* @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+        return brandService.findPage(page, rows);
+    }*/
 
     @PostMapping("/add")
     public Result add(@RequestBody TbBrand brand) {
         try {
             brandService.add(brand);
-            return Result.ok("新增成功");     } catch (Exception e) {
+            return Result.ok("新增成功");
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return Result.fail("新增失败"); }
+        return Result.fail("新增失败");
+    }
 
     @GetMapping("/findOne")
     public TbBrand findOne(Long id) {
@@ -44,21 +60,22 @@ public class BrandController {
     public Result update(@RequestBody TbBrand brand) {
         try {
             brandService.update(brand);
-            return Result.ok("修改成功");     } catch (Exception e) {
+            return Result.ok("修改成功");
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return Result.fail("修改失败");
     }
 
-    @GetMapping("/findAll")
-    public List<TbBrand> findAll() {
-//        return brandService.findAll();
-        return (List<TbBrand>) brandService.findAll();
+    @GetMapping("/delete")
+    public Result delete(Long[] ids) {
+        try {
+            brandService.deleteByIds(ids);
+            return Result.ok("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("删除失败");
     }
 
-    @GetMapping("/testPage")
-    public List<TbBrand> testPage(Integer page, Integer rows) {
-//        return brandService.testPage(page, rows);
-        return (List<TbBrand>) brandService.findPage(page, rows).getRows();
-    }
 }
